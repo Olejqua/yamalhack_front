ea575f05c0b0e899b92ea1bfaeab607791aa778c
Oleg <mfzpro@gmail.com>
2019-10-22 16:15:49 +0300

    added dict

--- src/panels/Create.js
+++ src/panels/Create.js
@@ -11,6 +11,8 @@ import HeaderButton from '@vkontakte/vkui/dist/components/HeaderButton/HeaderBut
 import Icon28ChevronBack from '@vkontakte/icons/dist/28/chevron_back';
 import Icon24Back from '@vkontakte/icons/dist/24/back';
 
+import { eventVariable } from '../static/dictionaries';
+
 const osName = platform();
 
 class Create extends React.Component {
@@ -20,11 +22,9 @@ class Create extends React.Component {
     this.state = {
       purpose: ''
     }
-
-    this.onChange = this.onChange.bind(this);
   }
 
-  onChange(e) {
+  onChange = e => {
     const { name, value } = e.currentTarget;
     this.setState({ [name]: value });
   }
@@ -52,27 +52,13 @@ class Create extends React.Component {
             value={purpose}
             name="purpose"
           >
-            <option value="0">Хакатон</option>
-            <option value="1">Социально-психологическая поддержка</option>
-            <option value="2">Профилактика здорового и безопасного образа жизни</option>
-            <option value="3">Педагогическое сопровождение</option>
-            <option value="4">Социальное патронирование детских домов</option>
-            <option value="5">Досуговая деятельность</option>
-            <option value="6">Экологическая защита</option>
-            <option value="7">Интеллектуальное развитие</option>
-            <option value="8">Творческое развитие</option>
-            <option value="9">Информационное обеспечение</option>
-            <option value="10">Медицинская помощь</option>
-            <option value="11">Спортивная, туристическая и военная подготовка</option>
-            <option value="12">Социальное патронирование пожилых людей</option>
-            <option value="13">Реставрационная деятельность</option>
-            <option value="14">Экскурсионно-паломническая деятельность</option>
-            <option value="15">Трудовая помощь</option>
-            <option value="16">Ремесленные мастерские</option>
-            <option value="17">Социальное краеведение</option>
-            <option value="18">Муниципальное управление</option>
-            <option value="19">Участие в выполнении специальных работ</option>
-            <option value="20">Иные виды работ</option>
+            {eventVariable.map((category, idx) => (
+              <option
+              value={idx}
+              >
+                {category}
+              </option>
+            ))}
           </Select>
           <Input top="Название мероприятия" />
           <Textarea top="Описание" />
--- src/panels/Home.js
+++ src/panels/Home.js
@@ -1,40 +1,81 @@
-import React from 'react';
+import React, { useState} from 'react';
 import PropTypes from 'prop-types';
 import Panel from '@vkontakte/vkui/dist/components/Panel/Panel';
 import PanelHeader from '@vkontakte/vkui/dist/components/PanelHeader/PanelHeader';
 import Button from '@vkontakte/vkui/dist/components/Button/Button';
+import Tabs from '@vkontakte/vkui/dist/components/Tabs/Tabs';
+import TabsItem from '@vkontakte/vkui/dist/components/TabsItem/TabsItem';
 import Group from '@vkontakte/vkui/dist/components/Group/Group';
 import Cell from '@vkontakte/vkui/dist/components/Cell/Cell';
 import Div from '@vkontakte/vkui/dist/components/Div/Div';
 import Avatar from '@vkontakte/vkui/dist/components/Avatar/Avatar';
+import FixedLayout from '@vkontakte/vkui/dist/components/FixedLayout/FixedLayout';
+import FormLayout from '@vkontakte/vkui/dist/components/FormLayout/FormLayout';
+import Select from '@vkontakte/vkui/dist/components/Select/Select';
 
-const Home = ({ id, go, fetchedUser }) => (
-	<Panel id={id}>
-		<PanelHeader>Example</PanelHeader>
-		{fetchedUser &&
-		<Group title="User Data Fetched with VK Connect">
-			<Cell
-				before={fetchedUser.photo_200 ? <Avatar src={fetchedUser.photo_200}/> : null}
-				description={fetchedUser.city && fetchedUser.city.title ? fetchedUser.city.title : ''}
-			>
-				{`${fetchedUser.first_name} ${fetchedUser.last_name}`}
-			</Cell>
-		</Group>}
+import { eventVariable } from '../static/dictionaries';
 
-		<Group title="Navigation Example">
-			<Div>
-				<Button size="xl" level="2" onClick={go} data-to="persik">
-					Show me the Persik, please
-				</Button>
-			</Div>
-			<Div>
-				<Button size="xl" level="2" onClick={go} data-to="create">
-					Create event
-				</Button>
-			</Div>
-		</Group>
-	</Panel>
-);
+
+const Home = ({ id, go, fetchedUser }) => {
+	const [activeTab, setActiveTab] = useState('volonture');
+
+	return (
+		<Panel id={id}>
+			<PanelHeader>
+				Меро
+			</PanelHeader>
+			<FormLayout>
+				<Select top="Категория" placeholder="Выберите категорию">
+					{eventVariable.map((category, idx) => (
+						<option
+						value={idx}
+						>
+							{category}
+						</option>
+					))}
+				</Select>
+    	</FormLayout>
+			{fetchedUser &&
+			<Group title="User Data Fetched with VK Connect">
+				<Cell
+					before={fetchedUser.photo_200 ? <Avatar src={fetchedUser.photo_200}/> : null}
+					description={fetchedUser.city && fetchedUser.city.title ? fetchedUser.city.title : ''}
+				>
+					{`${fetchedUser.first_name} ${fetchedUser.last_name}`}
+				</Cell>
+			</Group>}
+	
+			<Group title="Navigation Example">
+				<Div>
+					<Button size="xl" level="2" onClick={go} data-to="persik">
+						Show me the Persik, please
+					</Button>
+				</Div>
+				<Div>
+					<Button size="xl" level="2" onClick={go} data-to="create">
+						Create event
+					</Button>
+				</Div>
+			</Group>
+			<FixedLayout vertical="bottom">
+				<Tabs>
+					<TabsItem
+						onClick={() => setActiveTab('volonture')}
+						selected={activeTab === 'volonture'}
+					>
+						Волонтер
+					</TabsItem>
+					<TabsItem
+						onClick={() => setActiveTab('organiser')}
+						selected={activeTab === 'organiser'}
+					>
+						Организатор
+					</TabsItem>
+				</Tabs>
+			</FixedLayout>
+		</Panel>
+	);
+}
 
 Home.propTypes = {
 	id: PropTypes.string.isRequired,
